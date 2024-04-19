908006ec05c08d7ace7f4188e1ae077ab1fe2434
lucasgaldinodev <lucasgaldino7@gmail.com>
2024-04-19 19:38:38 -0300

    1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  // alterar aqui
+  let variavel: boolean = true
+
+  function renderizarConteudo(variavel: boolean) {
+    if (variavel) {
+      return (
+        <div className="flex justify-center items-center flex-1 p-5">
+          <Image src="/images/meme-1.jpeg" alt="" width={500} height={300} />
+          <Image src="/images/meme-2.png" alt="" width={300} height={300} />
+          <Image src="/images/meme-3.png" alt="" width={300} height={300} />
+          <Image src="/images/meme-4.jpeg" alt="" width={500} height={300} />
+        </div >
+      )
+    } else {
+      return (
+        <div className="flex justify-center items-center flex-1 p-5">
+          <Image src="/images/meme-extra.jpeg" alt="" width={1000} height={1000} />
+        </div >
+      )
+    }
+  }
+
   return (
     <main className="flex flex-col h-screen">
-      {/* cabeçalho */}
       <div className="flex justify-center items-center bg-[rgb(0,74,247)]" >
-        <Image src="/images/logo.png" alt="" width={200} height={300} />
+        <Image src="/images/unifor-logo.png" alt="" width={200} height={300} />
       </div >
-
-      {/* conteúdo */}
-      <div className="flex justify-center items-center flex-1 p-5">
-        <Image src="/images/meme1.jpeg" alt="" width={500} height={300} />
-        <Image src="/images/meme2.png" alt="" width={300} height={300} />
-        <Image src="/images/meme3.png" alt="" width={300} height={300} />
-        <Image src="/images/meme4.jpeg" alt="" width={500} height={300} />
-      </div >
-
-      {/* rodapé */}
+      {renderizarConteudo(variavel)}
       <div className="flex justify-center items-center h-20 bg-[rgb(0,74,247)]" >
         <a className="text-white text-5xl">Introdução a DevOps</a>
       </div >
